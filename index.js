897ceb699b037efdd1e92972fd8734edeab94478
Karthik Roop Kumar <karthikroopkumar@houzz.com>
2016-01-03 22:25:47 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+
+app.get('/', function(request, response) {
+    response.setHeader('Content-Type', 'application/json');
+    response.send({
+        title: 'fizzbuzz'
+    });
+});
+
+app.listen(8080);
+console.log('App listening on port %d', 8080);
