b9689057fd6a028a2b15f02a770733f66989b24e
Karthik Roop Kumar <karthikroopkumar@houzz.com>
2016-03-10 10:26:58 -0800

    testing

--- gitUtils/install.sh
+++ gitUtils/install.sh
@@ -2,12 +2,7 @@
 
 script_dir=$(dirname $0)
 
-if [[ -z "$1" || "$1" = "-h" || "$1" = "--help" ]]; then
-    echo "Usage: install.sh /path/to/git/repo" >&2
-    exit 1
-fi
-
-git_dir="$1/.git"
+git_dir="./.git"
 
 if [[ ! -d "$git_dir" ]]; then
     echo "$1 is not a git directory." >&2
@@ -17,6 +12,7 @@ fi
 copy_hook() {
     local from="$script_dir/$1"
     local to="$git_dir/hooks/$2"
+    touch $to
     echo "Copying $from to $to" >&2
     cp $from $to
     chmod +x $to
